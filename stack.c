101acb4350f70787e8d9ee50eac30f85d7fcef5d
J S <Joshuajcsmith@icloud.com>
2024-09-06 20:17:57 +0100

    Enhanced stack a bit more

--- stack.c
+++ stack.c
@@ -26,32 +26,44 @@ typedef struct{
 Stack CreateStack(int size,int numArgs,...){
     if (numArgs > size){
         printf("Stack Overflow");
-        return NULL;
-    }
-    //Called tmpstack since will be returned and given a name on return
-    Stack tmpstack;
-    tmpstack.max = size;
-    va_list args;
-    va_start(args,numArgs);
-    int *head = calloc(size,sizeof(int)); // *head = calloc(size,sizeof(int)); || head[size]; ?
+    } else{
+        //Called tmpstack since will be returned and given a name on return
+        Stack tmpstack;
+        tmpstack.max = size;
+        tmpstack.top = numArgs - 1;
+        va_list args;
+        va_start(args,numArgs);
+        int *head = calloc(size,sizeof(int)); // *head = calloc(size,sizeof(int)); || head[size]; ?
 
-    for (int i = 0; i < numArgs; i++){
-        head[i] = va_arg(args,int);
+        for (int i = 0; i < numArgs; i++){
+            head[i] = va_arg(args,int);
+        }
+        va_end(args);
+        tmpstack.head = (int *)(head+numArgs-1);
+        return tmpstack;
     }
-    va_end(args);
-    tmpstack.head = (int *)(head+numArgs-1);
-    return tmpstack;
 }
 
 int Peek(Stack tmpstack){
     return *(tmpstack.head);
 }
 
-int Pop(int *head){
+int Pop(Stack tmpstack){
+    if (tmpstack.top == -1){
+        printf("Stack Underflow");
+    } else{
+        int returnval = *tmpstack.head;
+        tmpstack.top -= 1;
+        tmpstack.head -= 1;
+        return returnval;
+    }
+    
 }
 
 int main(){
     Stack stack1 = CreateStack(5,4,2,3,4,5);
-    printf("Peek: %d\n",Peek(stack1));
+    printf("Peek: %d %p\n",Peek(stack1),stack1.head);
+    printf("Pop: %d\n",Pop(stack1));
+    printf("Peek: %d, %d %p\n",Peek(stack1),stack1.top,stack1.head);
     return 0;
 }
