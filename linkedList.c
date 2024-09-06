6d09a20f4f79933416edda29f250542be134ee9c
J S <Joshuajcsmith@icloud.com>
2024-09-06 16:56:34 +0100

    Added more to Linked list Restricted to integer values since i am not good at unions, There was something stopping me but I forgot Made some nice functions in Linked list

--- linkedList.c
+++ linkedList.c
@@ -1,25 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 
+/*
 typedef union {
     int intval;
-    char str[100];
+    char str[50];
     char charval;
     float floatval;
-} elementtype;
-
+} data;
+*/
 typedef struct Node{
     struct Node *next;
-    elementtype element;
+    int data;
 } Node;
 
+Node* LinkedList(int numArgs, ...){
+    va_list args;
+    va_start(args, numArgs);
+    int dataArg = va_arg(args, int);
+
+    Node *head = (Node *)malloc(sizeof(Node));
+    head->data = dataArg;
+    Node *current = head;
+
+    for (int i = 1; i < numArgs; i++){
+        Node *newNode = (Node *)malloc(sizeof(Node));
+        newNode->data = va_arg(args, int);
+        current->next = newNode;
+        current = newNode;
+    }
+
+    current->next = NULL;
+    va_end(args);
+    return head;
+}
+
+void DisplayList(Node *head){
+    Node *current = head;
+    while (current!=NULL){
+        printf("%d -> ",current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
 int main(){
-    Node Node1;
-    Node Node2;
-    Node Node3;
-    Node1.element.intval = 69;
-    Node1.next = &Node3;
-    Node3.next = &Node2;
-    printf("%d %p ",Node1.element.intval,*Node1.next);
+    Node *LL1head = LinkedList(5,1,2,3,4,5);
+    DisplayList(LL1head);
     return 0;
 }
